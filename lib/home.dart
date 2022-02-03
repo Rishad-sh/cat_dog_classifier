5c68be9392bd32d4365f3d1d40d06be0d06a786b
Rishad-sh <hshomoy@gmail.com>
2022-02-03 22:46:54 +0600

    First Commit

---
+++ lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'package:cat_dog_classifier/home.dart';
+class MySplash extends StatefulWidget {
+
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds:2,
+      navigateAfterSeconds: Home(),
+      title:Text('Cat and Dog Classifier',
+      style:TextStyle(
+        fontWeight: FontWeight.bold, 
+        fontSize: 25,
+        color:Color(0x00FFF))
+    ),
+    image: Image.asset('assets/cat_dog_icon.png'),
+    backgroundColor: Colors.blueAccent,
+    photoSize:  60,
+    loaderColor: Color(0x004242),
+
+    );
+  }
+}
